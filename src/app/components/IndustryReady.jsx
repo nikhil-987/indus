74a2256f7773febc725df64dfec4d1ba0a3ec639
nikhil-987 <nikhilkumargupta947@gmail.com>
2024-07-13 21:01:13 +0530

    changed image in industry ready component

--- src/app/components/IndustryReady.jsx
+++ src/app/components/IndustryReady.jsx
@@ -7,8 +7,8 @@ const IndustryReady = () => {
   <div class="mx-auto grid max-w-screen-xl rounded-lg bg-gray-50 p-4 dark:bg-gray-800 md:p-8 lg:grid-cols-12 lg:gap-8 lg:p-16 xl:gap-16">
     <div class="lg:col-span-5 lg:mt-0">
       <a href="#">
-        <img class="mb-4 h-56 w-56 dark:hidden sm:h-96 sm:w-96 md:h-full md:w-full" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-components.svg" alt="peripherals" />
-        <img class="mb-4 hidden dark:block md:h-full" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-components-dark.svg" alt="peripherals" />
+        <img class="mb-4 h-56 w-56 dark:hidden sm:h-96 sm:w-96 md:h-full md:w-full" src="6670895.jpg" alt="peripherals" />
+        {/* <img class="mb-4 hidden dark:block md:h-full" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-components-dark.svg" alt="peripherals" /> */}
       </a>
     </div>
     <div class="me-auto place-self-center lg:col-span-7">
