b931a0a3790cc819108947fa7423c49be4b30890
nikhil-987 <nikhilkumargupta947@gmail.com>
2024-07-14 11:56:27 +0530

    made changes to industry ready component

--- src/app/components/IndustryReady.jsx
+++ src/app/components/IndustryReady.jsx
@@ -2,20 +2,9 @@ import React from "react";
 
 const IndustryReady = () => {
   return (
-    <>
-      <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
-        {/* image start */}
-        <a href="#">
-          <img
-            class="mb-4 h-56 w-56 dark:hidden sm:h-96 sm:w-96 md:h-full md:w-full"
-            src="images/6670985.jpg"
-            alt="peripherals"
-          />
-          {/* <img class="mb-4 hidden dark:block md:h-full" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-components-dark.svg" alt="peripherals" /> */}
-        </a>
-        {/* image end */}
-
-        {/* description start */}
+    <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
+      {/* explore start */}
+      <div className="flex flex-col justify-center items-center p-10 order-1 md:order-2">
         <h1 class="mb-3 text-2xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-4xl">
           Save $500 today on your purchase <br />
           of a new iMac computer.
@@ -31,9 +20,26 @@ const IndustryReady = () => {
           {" "}
           Pre-order now{" "}
         </a>
-        {/* description end */}
       </div>
-    </>
+      {/* explore end */} {/* login start */}
+      <div className="bg-white p-10 order-2 md:order-1">
+        <a href="#">
+          <img
+            class="mb-4 h-56 w-56 dark:hidden sm:h-96 sm:w-96 md:h-full md:w-full"
+            src="images/6670985.jpg"
+            alt="peripherals"
+          />
+          {/*
+      <img
+        class="mb-4 hidden dark:block md:h-full"
+        src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-components-dark.svg"
+        alt="peripherals"
+      />
+      */}
+        </a>
+      </div>
+      {/* login end */}
+    </div>
   );
 };
 
